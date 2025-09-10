1ecbd11b1ca946253d38eb3e44e2ef914091b52c
hakenshi <fkdias025@gmail.com>
2025-09-10 14:33:22 -0300

    fix: clean up imports and remove commented-out code in Header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,7 @@
+import { AlertTriangle, FolderIcon, Handshake, HomeIcon, Info, Mail } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
 import { buttonVariants } from './ui/button'
-import { Info, Calendar, Mail, Handshake, AlertTriangle, HomeIcon, FolderIcon } from 'lucide-react'
 
 export default function Header() {
     return (
@@ -26,14 +25,14 @@ export default function Header() {
                             </span>
                         </Link>
                     </li>
-                    <li>
+                    {/* <li>
                         <Link href="/#eventos" className="text-gray-600 hover:text-pink-600 transition duration-300 flex items-center">
                             <span className="hidden md:inline">Eventos</span>
                             <span className="md:hidden text-lg">
                                 <Calendar className="w-5 h-5" />
                             </span>
                         </Link>
-                    </li>
+                    </li> */}
                     <li>
                         <Link href="/projetos" className="text-gray-600 hover:text-pink-600 transition duration-300 flex items-center">
                             <span className="hidden md:inline">Projetos</span>
