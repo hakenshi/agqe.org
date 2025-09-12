5b7b5c63042618fc7f7bbd86d14e65e17a0a2458
Felipe <111458342+hakenshi@users.noreply.github.com>
2025-09-11 21:04:23 -0300

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,6 +5,7 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export function getFileURL(key?: string) {
-  return key != undefined ? `${process.env.NEXT_PUBLIC_CLOUDFLARE_R2_ENDPOINT}/${key}` : null
+export function getFileURL(key: string) {
+  const endpoint = process.env.NEXT_PUBLIC_CLOUDFLARE_R2_ENDPOINT;
+  return key.includes(endpoint) ? key : `${process.env.NEXT_PUBLIC_CLOUDFLARE_R2_ENDPOINT}/${key}`
 }
